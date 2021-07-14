81fd5ccbacdee26a29d7e2242a81866bb124fcc1
Piotr Szok <piotr.szok@gmail.com>
2021-07-14 14:58:19 +0200

    Calculator Angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DigitButtonComponent } from './digit-button/digit-button.component';
+import { OpButtonComponent } from './op-button/op-button.component';
+import { DisplayComponent } from './display/display.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DigitButtonComponent,
+    OpButtonComponent,
+    DisplayComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule
